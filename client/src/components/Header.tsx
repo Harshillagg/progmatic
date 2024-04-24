f74a63ef469813b7b9feb2105843466a742354ec
Ishaan Minocha <minochaishaan2003@gmail.com>
2024-04-25 03:39:10 +0530

    fe setup done

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,32 +1,38 @@
+import { useState } from "react";
 import { Avatar, Dropdown, Navbar } from "flowbite-react";
 import { NavLink, useLocation } from "react-router-dom";
 
 export default function Header() {
   const location = useLocation();
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  const handleLoginWithGitHub = () => {
+  };
 
   return (
     <Navbar className="bg-[#181818] h-20 flex items-center justify-between">
       <Navbar.Brand href="/">
-        {/* <img src="/logo.jpg" className="mr-3 h-6 sm:h-9" alt="Logo" /> */}
         <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">&lt; progmatic / &gt;</span>
       </Navbar.Brand>
       <div className="flex md:order-2">
-        <Dropdown
-          arrowIcon={false}
-          inline
-          label={
-            <Avatar alt="User settings" img="/avatar.jpg" rounded />
-          }
-        >
-          <Dropdown.Header>
-            <span className="block text-sm">Ishaan Minocha</span>
-            <span className="block truncate text-sm font-medium">minochaishaan2003@gmail.com</span>
-          </Dropdown.Header>
-          <Dropdown.Item>Dashboard</Dropdown.Item>
-          <Dropdown.Item>UI Settings</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item>Sign out</Dropdown.Item>
-        </Dropdown>
+        {isLoggedIn ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={<Avatar alt="User settings" img="/avatar.jpg" rounded />}
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">Ishaan Minocha</span>
+              <span className="block truncate text-sm font-medium">minochaishaan2003@gmail.com</span>
+            </Dropdown.Header>
+            <Dropdown.Item>Dashboard</Dropdown.Item>
+            <Dropdown.Item>UI Settings</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <button className="text-white text-base hover:underline" onClick={handleLoginWithGitHub}>Login with GitHub</button>
+        )}
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
