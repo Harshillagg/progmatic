7ca2e4395cfa756ab86a859b4dfb701d628cc13d
Ishaan Minocha <minochaishaan2003@gmail.com>
2024-04-29 04:53:11 +0530

    cors error correction

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,6 +11,14 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+
 app.get('/', (req, res) => {
   res.send('Server Running!')
 })
