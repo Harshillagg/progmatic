633567bbbe8a684af75de09c72c585f26830aede
blood <chayanmann09@gmail.com>
2024-05-08 03:25:34 +0530

    data model files and util files

---
+++ server/src/models/user.model.ts
@@ -0,0 +1,68 @@
+import mongoose, { Document, Schema } from 'mongoose';
+import jwt from "jsonwebtoken"
+import {RoleDocument} from "./role.model.js"
+
+interface UserDocument extends Document {
+  gitHubUsername: string;
+  accessToken: string;
+  profilePhoto: string;
+  role: RoleDocument['_id']
+  contests: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const UserSchema: Schema<UserDocument> = new Schema(
+  {
+    _id: 
+    { 
+        type: mongoose.Schema.Types.ObjectId, 
+        required: true 
+    },
+    gitHubUsername: 
+    { 
+        type: String, 
+        required: true 
+    },
+    accessToken: 
+    { 
+        type: String, 
+        required: true 
+    },
+    profilePhoto: 
+    { 
+        type: String, 
+        required: true 
+    },
+    role: 
+    { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Role', 
+        required: true 
+    },
+    contests: 
+    { 
+        type: Number, 
+        default: 0 
+    },
+  },
+
+    { 
+    timestamps: true 
+    }
+);
+
+
+// UserSchema.methods.generateAccessToken = function(): string {
+//     return jwt.sign({
+//             _id: this._id,
+//         },
+//             process.env.ACCESS_TOKEN_SECRET,
+//         {
+//             expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+//         }
+//     );
+// }
+
+const UserModel = mongoose.model<UserDocument>('User', UserSchema);
+export default UserModel;
