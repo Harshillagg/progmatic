31bf2d21146f915ead44fa53a1854efd52ab88dc
Harshill Aggarwal <harshillsms@gmail.com>
2024-05-25 21:52:27 +0530

    add Contest Page

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -2,7 +2,7 @@ import Card from "./Card"
 
 function hero() {
   return (
-    <div className='w-screen bg-[url(OIG.jpg)] bg-center bg-cover bg-no-repeat bg-fixed bg-blend-multiply bg-gray-900 bg-opacity-95'>
+    <div className='w-screen bg-[url("/OIG.jpg")] bg-center bg-cover bg-no-repeat bg-fixed bg-blend-multiply bg-gray-950 bg-opacity-95'>
       <div className=''>
         <Card />
       </div>
