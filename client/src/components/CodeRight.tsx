0b03e78ef5d17e6aed5b2e59d7c17424d43335e3
PrathamGupta06 <87975651+PrathamGupta06@users.noreply.github.com>
2024-05-30 22:16:23 +0530

    Reizable Panels Now Work

--- client/src/components/CodeRight.tsx
+++ client/src/components/CodeRight.tsx
@@ -1,23 +1,30 @@
 import { useSharedState } from './SharedStateContext';
-
+import Split from '@uiw/react-split';
 const CodeRight: React.FC = () => {
   const { programOutput } = useSharedState();
   const {programInput} = useSharedState();
 
+// <div className="pt-20 text-black bg-white	">
+//    Output: <pre>{programOutput}</pre>
+// </div>
+// </div>
+
   return (
-    <>
-      <div className="pt-20 text-black">
-        {/* Input text box */}
+    <div style={{width:'100%', height:'100%'}} className="pt-20 text-black">
+      <Split mode='vertical'>
+      <div style={{height:'50%'}} >
         <textarea
-          className="w-full h-32 p-2"
+          className="w-full p-2"
+          style={{height: '100%', resize: 'none'}}
           placeholder="Enter input here"
           onChange={(e) => programInput.current = e.target.value}
         />
       </div>
-      <div className="pt-20 text-black">
-          Output: <pre>{programOutput}</pre>
+      <div style={{height:'50%'}} className="pl-2 bg-white">
+        Output: <pre>{programOutput}</pre>
       </div>
-    </>
+      </Split>
+    </div>
   );
 };
 
--- client/src/pages/Code.tsx
+++ client/src/pages/Code.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import ResizablePanels from '../components/ResizablePanels';
 import CodeLeft from '../components/CodeLeft';
 import CodeRight from '../components/CodeRight';
 import { SharedStateProvider } from '../components/SharedStateContext';
+import Split from '@uiw/react-split';
 
 const Code: React.FC = () => {
     return (
         <SharedStateProvider>
-        <div className="h-screen">
-            <ResizablePanels
-                leftComponent={() => <CodeLeft />}
-                rightComponent={() => <CodeRight />}
-            />
-        </div>
+            <Split style={{ height: '100%', width: '100%', border: '1px solid #d5d5d5', borderRadius: 3 }}>
+            <div style={{width:'50%'}}>
+                <CodeLeft />
+            </div>
+            <div style={{width:'50%'}}>
+                <CodeRight />
+            </div>
+            </Split>
         </SharedStateProvider>
     );
 };
